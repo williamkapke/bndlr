24a405540e9d4b5e885b419131c178644447dadc
William Wicks <wjwicks@gmail.com>
2012-03-01 09:56:11 -0800

    adding examples

--- example/example.js
+++ example/example.js
@@ -1,6 +1,11 @@
 
 var bndlr = require("./../index.js"),
-	path = require('path');
+	path = require('path'),
+	uglify = require('uglify-js'),
+	jsparser = uglify.parser,
+	jsprocessor = uglify.uglify,
+	cleancss = require('clean-css')
+	;
 
 bndlr.pathModifier = function(configdir, type, filename){
 	//interpret where the files are stored locally
@@ -16,11 +21,24 @@ bndlr.Bundle.prototype.uniqueFileNamer = function(){
 	//configure the url the bundle is located at
 	return '/example/' + this.md5 + '.' + this.type;
 };
+bndlr.StaticFile.compressors.js = function(content){
+	var ast = jsparser.parse(content); // parse code and get the initial AST
+	ast = jsprocessor.ast_mangle(ast); // get a new AST with mangled names
+	ast = jsprocessor.ast_squeeze(ast); // get an AST with compression optimizations
+	var final_code = jsprocessor.gen_code(ast); // compressed code here
+	return final_code;
+}
+bndlr.StaticFile.compressors.css = function(content){
+	return cleancss.process(content);
+}
 
 //open a config
 var	cfg = bndlr.open(__dirname+'/example.config.js');
 
 
-cfg.js.global.files.forEach(function(file){
+cfg.css.global.files.forEach(function(file){
+	var content = file.content;
+	var minified = file.minified;
 	console.log(file.uniqueFileName);
 });
+
---
+++ example/js/file1.js
@@ -0,0 +1,4 @@
+(function(){
+	var text = "This is content from file1.js";
+	console.log(text);
+})();
